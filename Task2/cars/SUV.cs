a88b509813ebf123f511b663db2e9d0c13a45959
MUSAB ALQARNI <78758932+MusabSwe@users.noreply.github.com>
2021-06-21 08:38:17 +0300

    Update SUV.cs
    create a car object and remove static method and fix toggle method

--- Task2/cars/SUV.cs
+++ Task2/cars/SUV.cs
@@ -4,20 +4,25 @@ namespace Task2
 {
     class SUV : Car, ISunRoof
     {
+          Car c1 = new Car();
+
         public void toggle()
         {
-            bool isOpen = false;
 
-            switch (isOpen)
+            if (c1.IsOpen == true)
             {
-                case true:
-                    Console.WriteLine("The sunroof of SUV car is open");
-                    break;
-                case false:
-                    Console.WriteLine("The sunroof of SUV car is closed");
-                    break;
+                Console.WriteLine("The sunroof of SUV car is open");
+                Console.WriteLine("off");
+                c1.IsOpen = false;
+            }
+            else if (c1.IsOpen == false)
+            {
+                Console.WriteLine("The sunroof of SUV car is closed");
+                Console.WriteLine("on");
+                c1.IsOpen = true;
             }
         }
+      
     }
 
 }
