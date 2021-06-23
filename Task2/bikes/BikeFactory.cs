9aaaac639fc9a006987e8d616d22715552ec162d
MUSAB ALQARNI <78758932+MusabSwe@users.noreply.github.com>
2021-06-23 17:00:00 +0300

    BikeFactory class

---
+++ Task2/bikes/BikeFactory.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Task2
+{
+    public class BikeFactory
+    {
+        public List<Bike> arrBike = new List<Bike>();
+
+        public BikeFactory(List<Bike> arr)
+        {
+            this.arrBike = arr;
+        }
+        public BikeFactory()
+        {
+            
+        }
+
+        public void AddBike(Bike arr)
+        {
+            arrBike.Add(new Bike(arr.name, arr.passengerNum, arr.numberOfWheels, arr.bikeType));
+        }
+
+        public void DB()
+        {
+            arrBike.Add(new Bike("Venom", 2, 2, "motor bike"));
+            arrBike.Add(new Bike("Rambo", 1, 2, "Bicycle"));
+       
+        }
+    }
+}
