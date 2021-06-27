3700f2f510addf0b00839fcfeb56772bbde83bb9
MUSAB ALQARNI <78758932+MusabSwe@users.noreply.github.com>
2021-06-27 10:29:11 +0300

    Refactor

--- Task2/cars/Car.cs
+++ Task2/cars/Car.cs
@@ -1,18 +1,26 @@
 using System;
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Task2
 {
-    class Car : Vehicle
+    public class Car : Vehicle
     {
-        private int passengerNum;
-        private int numberOfCylinders;
-        private int numberOfDoors;
-        private string name;
+        public string name { get; set; }
+
+        public int passengerNum { get; set; }
+        public int numberOfCylinders { get; set; }
+
+        public int numberOfDoors { get; set; }
+        public Wheel wheel = new Wheel();
+        public EngineType engine = new EngineType();
+        public FuelEconomy fuelEconomy = new FuelEconomy();
+        public CarFactory c = new CarFactory();
+
         // --------------------------------
         public bool IsOpen = false;
 
-        public Car(string n,int passengerNum, int numOfCylinders, int numOfDoors)
+        public Car(string n, int passengerNum, int numOfCylinders, int numOfDoors, EngineType e, Wheel w)
         {
             if (passengerNum > 0 && numOfCylinders > 0 && numOfDoors > 0)
             {
@@ -20,29 +28,68 @@ namespace Task2
                 this.passengerNum = passengerNum;
                 this.numberOfCylinders = numOfCylinders;
                 this.numberOfDoors = numOfDoors;
-
+                this.engine = e;
+                this.wheel = w;
             }
             else
             {
                 Console.WriteLine("Invalid input, the values should be > 0");
             }
+        }
 
-
+        public Car(string n, int passengerNum, int numOfCylinders, int numOfDoors, EngineType e, Wheel w, FuelEconomy f)
+        {
+            if (passengerNum > 0 && numOfCylinders > 0 && numOfDoors > 0)
+            {
+                this.name = n;
+                this.passengerNum = passengerNum;
+                this.numberOfCylinders = numOfCylinders;
+                this.numberOfDoors = numOfDoors;
+                this.engine = e;
+                this.wheel = w;
+                this.fuelEconomy = f;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input, the values should be > 0");
+            }
         }
 
         public Car()
         {
-
         }
-        public string NameProp { get; set; }
 
-        public int PassengerNumProp { get; set; }
+        public string toString(string w, string e = "Regular")
+        {
+            
+            c.ConnectToDB();
+            int j = 0;
+            for (int i = 0; i < c.wheels.Length; i++)
+            {
+                if (c.wheels[i].tireName.ToLower().Equals(w.ToLower()))
+                {
+                    j = i;
+                }
+            }
 
-        public int NumberOfCylindersProp { get; set; }
-
-        public int NumberOfDoorsProp { get; set; }
-
-
+            if (e.ToLower().Equals("regular"))
+            {
+                return "Specifications for " + name + " car are:\nnumber of passengers: " + passengerNum +
+                       "\nnumber of cylinders: " + numberOfCylinders + "\nnumber of doors: " + numberOfDoors +
+                       "\nThe engine type is Regular\n" + c.wheels[j].toString() + "\n" +
+                       fuelEconomy.toString();
+            }
+            else if (e.ToLower().Equals("hybrid"))
+            {
+                return "Specifications for " + name + " car are:\nnumber of passengers: " + passengerNum +
+                       "\nnumber of cylinders: " + numberOfCylinders + "\nnumber of doors: " + numberOfDoors +
+                       "\nThe engine type is  " + engine.EngineName + "\n" + c.wheels[j].toString();
+            }else
+            {
+                return "Specifications for " + name + " car are:\nnumber of passengers: " + passengerNum +
+                       "\nnumber of cylinders: " + numberOfCylinders + "\nnumber of doors: " + numberOfDoors +
+                       "\nThe engine type is Diesel\n" + c.wheels[j].toString();
+            }
+        }
     }
-
 }
