a46c66a942370f87b1ff366130b760577b1e6e07
MUSAB ALQARNI <78758932+MusabSwe@users.noreply.github.com>
2021-06-27 10:33:06 +0300

    Refactor

--- Task2/bikes/BikeFactory.cs
+++ Task2/bikes/BikeFactory.cs
@@ -1,30 +1,51 @@
-﻿using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 
 namespace Task2
 {
     public class BikeFactory
     {
         public List<Bike> arrBike = new List<Bike>();
-
+        public Wheel[] wheels = new Wheel[2];
         public BikeFactory(List<Bike> arr)
         {
             this.arrBike = arr;
         }
+
         public BikeFactory()
         {
-            
         }
 
         public void AddBike(Bike arr)
         {
-            arrBike.Add(new Bike(arr.name, arr.passengerNum, arr.numberOfWheels, arr.bikeType));
+            if (arr.engine.EngineName != null)
+            {
+                arrBike.Add(new Bike(arr.name, arr.passengerNum, arr.numberOfWheels, arr.bikeType, arr.engine,
+                    arr.wheel));
+            }
+            else if (arr.engine.EngineName.ToLower().Equals("regular"))
+            {
+                arrBike.Add(new Bike(arr.name, arr.passengerNum, arr.numberOfWheels, arr.bikeType, arr.engine,
+                    arr.wheel, arr.fuelEcon));
+            }
+            else
+            {
+                arrBike.Add(new Bike(arr.name, arr.passengerNum, arr.numberOfWheels, arr.bikeType, arr.wheel));
+            }
         }
 
-        public void DB()
+        public void ConnectToDB()
         {
-            arrBike.Add(new Bike("Venom", 2, 2, "motor bike"));
-            arrBike.Add(new Bike("Rambo", 1, 2, "Bicycle"));
-       
+            wheels[0] = new Wheel("Cobra", "German ", new DateTime(2021, 2, 22), 12);
+            wheels[1] = new Wheel("Kenda", "America ", new DateTime(2020, 9, 17), 9);
+
+            /*0*/
+            arrBike.Add(new Bike("Venom", 2, 2, "motor bike", new EngineType(2, "Regular"),
+                new Wheel("Cobra", "German ", new DateTime(2021, 2, 22), 12),
+                new FuelEconomy("Venom", new DateTime(2021, 3, 4), "Green", "Good")));
+            /*1*/
+            arrBike.Add(
+                new Bike("Rambo", 1, 2, "Bicycle", new Wheel("Kenda", "America ", new DateTime(2020, 9, 17), 9)));
         }
     }
 }
