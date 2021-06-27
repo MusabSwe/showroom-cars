32a3914db7de3585472b6ff9e15073dd475ae3d0
MUSAB ALQARNI <78758932+MusabSwe@users.noreply.github.com>
2021-06-27 10:30:49 +0300

    Refactor

--- Task2/bikes/Bike.cs
+++ Task2/bikes/Bike.cs
@@ -1,40 +1,105 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Task2
 {
-    class Bike
+    public class Bike
     {
-        private int passengerNum;
-        private int numberOfWheels;
-        private string bikeType;
+        public string name { get; set; }
 
-        public Bike(int passengerNum, int numOfWheels, string bikeType)
+        public int passengerNum { get; set; }
+
+        public int numberOfWheels { get; set; }
+
+        public string bikeType { get; set; }
+
+        public Wheel wheel = new Wheel();
+
+        public EngineType engine = new EngineType();
+        public FuelEconomy fuelEcon = new FuelEconomy();
+        public BikeFactory b = new BikeFactory();
+
+        public Bike(string name, int passengerNum, int numOfWheels, string bikeType, EngineType e, Wheel w,
+            FuelEconomy f)
         {
-
-            if (passengerNum > 0 && numOfWheels > 0 && (bikeType.ToLower().Equals("bicycle") || bikeType.ToLower().Equals("motorbike")))
+            if (passengerNum > 0 && numOfWheels > 0)
             {
-
+                this.name = name;
                 this.passengerNum = passengerNum;
                 this.numberOfWheels = numOfWheels;
                 this.bikeType = bikeType;
+                this.engine = e;
+                this.wheel = w;
+                this.fuelEcon = f;
             }
             else
             {
                 Console.WriteLine("Invalid input");
             }
-
         }
 
-        public Bike(){
-            
+        public Bike(string name, int passengerNum, int numOfWheels, string bikeType, Wheel w)
+        {
+            if (passengerNum > 0 && numOfWheels > 0)
+            {
+                this.name = name;
+                this.passengerNum = passengerNum;
+                this.numberOfWheels = numOfWheels;
+                this.bikeType = bikeType;
+                this.wheel = w;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
         }
 
-        public int PassengerNumProp { get; set; }
+        public Bike(string name, int passengerNum, int numOfWheels, string bikeType, EngineType e, Wheel w)
+        {
+            if (passengerNum > 0 && numOfWheels > 0)
+            {
+                this.name = name;
+                this.passengerNum = passengerNum;
+                this.numberOfWheels = numOfWheels;
+                this.bikeType = bikeType;
+                this.engine = e;
+                this.wheel = w;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+        }
 
-        public int NumberOfWheelsProp { get; set; }
+        public Bike()
+        {
+        }
 
-        public string BikeTypeProp { get; set; }
 
+        public string toString()
+        {
+            // b.ConnectToDB();
+            // int j = 0;
+            // for (int i = 0; i < b.wheels.Length; i++)
+            // {
+            //     if (b.wheels[i].tireName.ToLower().Equals(w.ToLower()))
+            //     {
+            //         j = i;
+            //     }
+            // }
 
+            if (this.bikeType.ToLower().Equals("bicycle"))
+            {
+                return "Specifications for " + name + " bike are:\nnumber of passengers: "
+                       + passengerNum + "\nnumber of wheels: " + numberOfWheels + "\nbike type: " + bikeType
+                       + "\n" + wheel.toString();
+            }
+            else
+            {
+                return "Specifications for " + name + " bike are:\nnumber of passengers: "
+                       + passengerNum + "\nnumber of wheels: " + numberOfWheels + "\nbike type: " + bikeType
+                       + "\nThe engine type is Regular\n" + wheel.toString();
+            }
+        }
     }
 }
