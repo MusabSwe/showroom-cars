790d785d145f9c4456b2af771fc950fb1681a7b3
MUSAB ALQARNI <78758932+MusabSwe@users.noreply.github.com>
2021-06-29 11:45:49 +0300

    Update Van.cs

--- Task2/cars/Van.cs
+++ Task2/cars/Van.cs
@@ -1,8 +1,60 @@
 namespace Task2
 {
-    class Van : Car
+    public class Van : Car
     {
-       
-    }
+        public Van(string n, int passNum, int cylindNum, int doorNum, EngineType e, Wheel w, FuelEconomy f)
+        {
+            this.name = n;
+            this.passengerNum = passNum;
+            this.numberOfCylinders = cylindNum;
+            this.numberOfDoors = doorNum;
+            this.engine = e;
+            this.wheel = w;
+            this.fuelEconomy = f;
+        }
 
+        public Van(string n, int passNum, int cylindNum, int doorNum, EngineType e, Wheel w)
+        {
+            this.name = n;
+            this.passengerNum = passNum;
+            this.numberOfCylinders = cylindNum;
+            this.numberOfDoors = doorNum;
+            this.engine = e;
+            this.wheel = w;
+        }
+        public string toString(string w, string e = "Regular")
+        {
+            CarFactory c = new CarFactory();
+            c.ConnectToDB();
+            int j = 0;
+
+            for (int i = 0; i < c.wheels.Length; i++)
+            {
+                if (c.wheels[i].tireName.ToLower().Equals(w.ToLower()))
+                {
+                    j = i;
+                }
+            }
+
+            if (e.ToLower().Equals("regular"))
+            {
+                return "Specifications for " + name + " sedan car are:\nnumber of passengers: " + passengerNum +
+                       "\nnumber of cylinders: " + numberOfCylinders + "\nnumber of doors: " + numberOfDoors +
+                       "\nThe engine type is Regular\n" + c.wheels[j].toString() + "\n" +
+                       fuelEconomy.toString();
+            }
+            else if (e.ToLower().Equals("hybrid"))
+            {
+                return "Specifications for " + name + " van car are:\nnumber of passengers: " + passengerNum +
+                       "\nnumber of cylinders: " + numberOfCylinders + "\nnumber of doors: " + numberOfDoors +
+                       "\nThe engine type is  " + engine.EngineName + "\n" + c.wheels[j].toString();
+            }
+            else
+            {
+                return "Specifications for " + name + " car are:\nnumber of passengers: " + passengerNum +
+                       "\nnumber of cylinders: " + numberOfCylinders + "\nnumber of doors: " + numberOfDoors +
+                       "\nThe engine type is Diesel\n" + c.wheels[j].toString();
+            }
+        }
+    }
 }
