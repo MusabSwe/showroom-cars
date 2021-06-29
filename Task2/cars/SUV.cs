f037fc1b6cfa0c436ad6b5e51a9e37c652d95e57
MUSAB ALQARNI <78758932+MusabSwe@users.noreply.github.com>
2021-06-29 11:43:35 +0300

    Update SUV.cs

--- Task2/cars/SUV.cs
+++ Task2/cars/SUV.cs
@@ -2,27 +2,78 @@ using System;
 
 namespace Task2
 {
-    class SUV : Car, ISunRoof
+    public class Sedan : Car, ISunRoof
     {
-          Car c1 = new Car();
+        Car c1 = new Car();
+
+        public Sedan(string n, int passNum, int cylindNum, int doorNum, EngineType e, Wheel w, FuelEconomy f)
+        {
+            this.name = n;
+            this.passengerNum = passNum;
+            this.numberOfCylinders = cylindNum;
+            this.numberOfDoors = doorNum;
+            this.engine = e;
+            this.wheel = w;
+            this.fuelEconomy = f;
+        }
+
+        public Sedan(string n, int passNum, int cylindNum, int doorNum, EngineType e, Wheel w)
+        {
+            this.name = n;
+            this.passengerNum = passNum;
+            this.numberOfCylinders = cylindNum;
+            this.numberOfDoors = doorNum;
+            this.engine = e;
+            this.wheel = w;
+        }
+
 
         public void toggle()
         {
-
             if (c1.IsOpen == true)
             {
-                Console.WriteLine("The sunroof of SUV car is open");
+                Console.WriteLine("The sunroof of Sedan car is open");
                 Console.WriteLine("off");
                 c1.IsOpen = false;
             }
             else if (c1.IsOpen == false)
             {
-                Console.WriteLine("The sunroof of SUV car is closed");
+                Console.WriteLine("The sunroof of Sedan car is closed");
                 Console.WriteLine("on");
                 c1.IsOpen = true;
             }
         }
-      
-    }
 
+        public string toString(string w, string e = "Regular")
+        {
+            CarFactory c = new CarFactory();
+            c.ConnectToDB();
+            int j = 0;
+            for (int i = 0; i < c.wheels.Length; i++)
+            {
+                if (c.wheels[i].tireName.ToLower().Equals(w.ToLower()))
+                {
+                    j = i;
+                }
+            }
+
+            if (e.ToLower().Equals("regular"))
+            {
+                return "Specifications for " + name + " sedan car are:\nnumber of passengers: " + passengerNum +
+                       "\nnumber of cylinders: " + numberOfCylinders + "\nnumber of doors: " + numberOfDoors +
+                       "\nThe engine type is Regular\n" + c.wheels[j].toString() + "\n" +
+                       fuelEconomy.toString();
+            }
+            else if (e.ToLower().Equals("hybrid"))
+            {
+                return "Specifications for " + name + " sedan car are:\nnumber of passengers: " + passengerNum +
+                       "\nnumber of cylinders: " + numberOfCylinders + "\nnumber of doors: " + numberOfDoors +
+                       "\nThe engine type is  " + engine.EngineName + "\n" + c.wheels[j].toString();
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
 }
