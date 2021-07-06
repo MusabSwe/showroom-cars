1012bbaebff63f235b01c0e2ab708b51af580591
MUSAB ALQARNI <78758932+MusabSwe@users.noreply.github.com>
2021-07-06 16:49:26 +0300

    Entity

--- Task2/cars/Car.cs
+++ Task2/cars/Car.cs
@@ -6,29 +6,30 @@ namespace Task2
 {
     public class Car : Vehicle
     {
-        public string name { get; set; }
-        public int passengerNum { get; set; }
-        public int numberOfCylinders { get; set; }
-        public int numberOfDoors { get; set; }
-        public Wheel wheel = new Wheel();
-        public EngineType engine = new EngineType();
-        public FuelEconomy fuelEconomy = new FuelEconomy();
+        public int CarId { get; set; }
+        public string Name { get; set; }
+        public int PassengerNum { get; set; }
+        public int NumberOfCylinders { get; set; }
+        public int NumberOfDoors { get; set; }
+        public Wheel Wheel = new Wheel();
+        public Engine Engines = new Engine();
+        public FuelEconomy FuelEconomy = new FuelEconomy();
 
         CarFactory c = new CarFactory();
 
         // --------------------------------
         public bool IsOpen = false;
 
-        public Car(string n, int passengerNum, int numOfCylinders, int numOfDoors, EngineType e, Wheel w)
+        public Car(string n, int passengerNum, int numOfCylinders, int numOfDoors, Engine e, Wheel w)
         {
             if (passengerNum > 0 && numOfCylinders > 0 && numOfDoors > 0)
             {
-                this.name = n;
-                this.passengerNum = passengerNum;
-                this.numberOfCylinders = numOfCylinders;
-                this.numberOfDoors = numOfDoors;
-                this.engine = e;
-                this.wheel = w;
+                this.Name = n;
+                this.PassengerNum = passengerNum;
+                this.NumberOfCylinders = numOfCylinders;
+                this.NumberOfDoors = numOfDoors;
+                this.Engines = e;
+                this.Wheel = w;
             }
             else
             {
@@ -36,17 +37,17 @@ namespace Task2
             }
         }
 
-        public Car(string n, int passengerNum, int numOfCylinders, int numOfDoors, EngineType e, Wheel w, FuelEconomy f)
+        public Car(string n, int passengerNum, int numOfCylinders, int numOfDoors, Engine e, Wheel w, FuelEconomy f)
         {
             if (passengerNum > 0 && numOfCylinders > 0 && numOfDoors > 0)
             {
-                this.name = n;
-                this.passengerNum = passengerNum;
-                this.numberOfCylinders = numOfCylinders;
-                this.numberOfDoors = numOfDoors;
-                this.engine = e;
-                this.wheel = w;
-                this.fuelEconomy = f;
+                this.Name = n;
+                this.PassengerNum = passengerNum;
+                this.NumberOfCylinders = numOfCylinders;
+                this.NumberOfDoors = numOfDoors;
+                this.Engines = e;
+                this.Wheel = w;
+                this.FuelEconomy = f;
             }
             else
             {
@@ -65,28 +66,28 @@ namespace Task2
             
             for (int i = 0; i < c.wheels.Length; i++)
             {
-                if (c.wheels[i].tireName.ToLower().Equals(w.ToLower()))
+                if (c.wheels[i].TireName.ToLower().Equals(w.ToLower()))
                 {
                     j = i;
                 }
             }
             if (e.ToLower().Equals("regular"))
             {
-                return "Specifications for " + name + " car are:\nnumber of passengers: " + passengerNum +
-                       "\nnumber of cylinders: " + numberOfCylinders + "\nnumber of doors: " + numberOfDoors +
+                return "Specifications for " + Name + " car are:\nnumber of passengers: " + PassengerNum +
+                       "\nnumber of cylinders: " + NumberOfCylinders + "\nnumber of doors: " + NumberOfDoors +
                        "\nThe engine type is Regular\n" + c.wheels[j].toString() + "\n" +
-                       fuelEconomy.toString();
+                       FuelEconomy.toString();
             }
             else if (e.ToLower().Equals("hybrid"))
             {
-                return "Specifications for " + name + " car are:\nnumber of passengers: " + passengerNum +
-                       "\nnumber of cylinders: " + numberOfCylinders + "\nnumber of doors: " + numberOfDoors +
-                       "\nThe engine type is  " + engine.EngineName + "\n" + c.wheels[j].toString();
+                return "Specifications for " + Name + " car are:\nnumber of passengers: " + PassengerNum +
+                       "\nnumber of cylinders: " + NumberOfCylinders + "\nnumber of doors: " + NumberOfDoors +
+                       "\nThe engine type is  " + Engines.EngineName + "\n" + c.wheels[j].toString();
             }
             else
             {
-                return "Specifications for " + name + " car are:\nnumber of passengers: " + passengerNum +
-                       "\nnumber of cylinders: " + numberOfCylinders + "\nnumber of doors: " + numberOfDoors +
+                return "Specifications for " + Name + " car are:\nnumber of passengers: " + PassengerNum +
+                       "\nnumber of cylinders: " + NumberOfCylinders + "\nnumber of doors: " + NumberOfDoors +
                        "\nThe engine type is Diesel\n" + c.wheels[j].toString();
             }
         }
