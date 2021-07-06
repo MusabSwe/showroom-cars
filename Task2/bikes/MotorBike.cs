58f038b2c961462daa9735b298d0a215599a0326
MUSAB ALQARNI <78758932+MusabSwe@users.noreply.github.com>
2021-07-06 16:45:51 +0300

    Update MotorBike.cs

--- Task2/bikes/MotorBike.cs
+++ Task2/bikes/MotorBike.cs
@@ -2,26 +2,14 @@ namespace Task2
 {
     class MotorBike : Bike
     {
-        private string Name;
+        
         private int speedTracker;
 
-        public MotorBike(string MotorName)
-        {
-
-            this.Name = MotorName;
-        }
-
-        public MotorBike()
-        {
-
-        }
-
-        public string NameProp { get; set; }
-        public int SpeedTracker { get; set; }
+        public int SpeedTrackerProp { get; set; }
 
         public int displaySpeed()
         {
-            return SpeedTracker;
+            return SpeedTrackerProp;
         }
     }
 }
