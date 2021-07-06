1d2f541f884ddf2b764682d49a7241fde4b738fe
MUSAB ALQARNI <78758932+MusabSwe@users.noreply.github.com>
2021-07-06 16:44:48 +0300

    Entity

--- Task2/base/Wheel.cs
+++ Task2/base/Wheel.cs
@@ -2,42 +2,35 @@ using System;
 
 namespace Task2
 {
-    class Wheel
+    public class Wheel
     {
+        public int WheelId { get; set; }
+        
+        public string TireName { get; set; }
 
-        private string tireName;
-        private string tireType;
-        private DateTime tireDate;
-        private double tireSize;
-        private string carName;
-        public Wheel(string carName, string TireName, string type, DateTime date, double size)
+        public string TireType { get; set; }
+
+        public DateTime TireDate { get; set; }
+
+        public double TireSize { get; set; }
+
+        public Wheel(string TireName, string type, DateTime date, double size)
         {
-
-            this.carName = carName;
-            this.tireName = TireName;
-            this.tireType = type;
-            this.tireDate = date;
-            this.tireSize = size;
+            this.TireName = TireName;
+            this.TireType = type;
+            this.TireDate = date;
+            this.TireSize = size;
         }
 
         public Wheel()
         {
-
         }
 
-        public string TireNameProp { get; set; }
-
-        public string TireTypeProp { get; set; }
-
-        public DateTime TireDateProp { get; set; }
-
-         public double TireSizeProp { get; set; }
 
         public string toString()
         {
-
-            return "Wheel information for " + carName + " car:\nName: " + TireNameProp + "\nDate: " + TireDateProp + "\nType: " + TireTypeProp + "\nSize: " + TireSizeProp;
+            return "Wheel information are :\nName: " + TireName + "\nDate: " + TireDate + "\nType: " + TireType +
+                   "\nSize: " + TireSize;
         }
-
     }
 }
